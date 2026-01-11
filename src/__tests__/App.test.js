70f08470b6045d78cbeca7ebf374638e13529ecf
Sal Estrada <sal.estrada21@gmail.com>
2026-01-10 23:10:45 -0500

    Update App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 //src/__tests__/App.test.js
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, within } from "@testing-library/react";
+import { render, within, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { getEvents } from "../api";
 import App from "../App";
