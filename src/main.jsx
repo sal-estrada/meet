15aea5b9868b62beb63c999bc0b2384466fe9238
Sal Estrada <sal.estrada21@gmail.com>
2026-01-21 16:48:31 -0500

    Add APM

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import * as atatus from 'atatus-spa';
+atatus.config('9f04e638d63f485a8a7ddf3d1fb89f2b').install();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
