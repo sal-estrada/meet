f6eb61e6a2abe0ae2b332faa31b34e70c0e2408a
Sal Estrada <sal.estrada21@gmail.com>
2026-01-21 17:37:25 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -65,7 +65,7 @@ const getToken = async (code) => {
   const { access_token } = await response.json();
   access_token && localStorage.setItem("access_token", access_token);
 
-  return access_token
+  return access_token;
 };
 
 export const getAccessToken = async () => {
