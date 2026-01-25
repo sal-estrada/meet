f6898301f901a07b8203b52340737b82f50bbd62
Sal Estrada <sal.estrada21@gmail.com>
2026-01-25 15:33:33 -0500

    Add alerts
    -Add user alerts if item does not exist.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
 import { extractLocations, getEvents } from "./api";
+import { InfoAlert } from "./components/Alert";
 
 import './App.css'
 
@@ -11,6 +12,7 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
 
 
   useEffect(() => {
@@ -44,7 +46,8 @@ const App = () => {
 
   return (
   <div className="App">
-    <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+    <div className="alerts-container"> {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}</div>
+    <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert}/>
     <NumberOfEvents onNumberChange={setCurrentNOE} />
     <EventList events={events} />
   </div>
