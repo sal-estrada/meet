fa482a1478e0baadf82a1d7f17e0f5506320a7f9
Sal Estrada <sal.estrada21@gmail.com>
2026-01-26 13:15:47 -0500

    Add PWA functionality

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
 import * as atatus from 'atatus-spa';
+import import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 atatus.config('9f04e638d63f485a8a7ddf3d1fb89f2b').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,3 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+serviceWorkerRegistration.register();
