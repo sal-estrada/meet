71d577f99818d8ed13b11b115e39594ba2c4940b
Sal Estrada <sal.estrada21@gmail.com>
2026-01-26 10:05:53 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
 import { extractLocations, getEvents } from "./api";
-import { InfoAlert, ErrorAlert } from "./components/Alert";
+import InfoAlert from "./components/Alert";
+import ErrorAlert from "./components/Alert";
 
 import './App.css'
 
