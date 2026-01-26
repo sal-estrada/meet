24010033551d01cbef87fc21d1298b3b903d257c
Sal Estrada <sal.estrada21@gmail.com>
2026-01-26 13:17:06 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import * as atatus from 'atatus-spa';
-import import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 atatus.config('9f04e638d63f485a8a7ddf3d1fb89f2b').install();
 
