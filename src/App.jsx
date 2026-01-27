ee73dd89b12232d37e629aa5d52ff84a195357f0
Sal Estrada <sal.estrada21@gmail.com>
2026-01-27 17:08:14 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -66,6 +66,9 @@ const App = () => {
 
   return (
     <div className="App">
+      <h1>Meet App</h1>
+      <p>Discover events happening in cities around the world.</p>
+
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
